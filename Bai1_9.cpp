0aa91efc93a849c3cf664124d114a2bc6c30bdcc
the-phong <phong245ntp@gmail.com>
2025-03-08 15:43:40 +0700

    Bai tap chuong 1

--- Bai1_9.cpp
+++ Bai1_9.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <map>
+
 using namespace std;
 
 int main(){
-    string s;
-    cin >> s;
-    cout << s;
+	string s; cin >> s;
+	int cnt=0;
+	map<char,int>mp;
+	for(int i=0;i<s.length()-1;i++){
+		for(int j=i+1;j<s.length();j++){
+			if(s[j]==s[i]){
+				for(int k=i+1;k<j;k++){
+					mp[s[k]]++;
+				}
+				for(auto x=mp.begin();x!=mp.end();x++){
+					if((*x).second==1){
+						cnt++;
+					}
+					(*x).second=0;
+				}
+				break;
+			}
+		}
+	}
+	cout << cnt/2;
 }
